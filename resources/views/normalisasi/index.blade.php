235c4c1eb6eb4d7b192032f1df396f8b5cd73f05
YayakFirmansyah <yakizam123@gmail.com>
2023-12-12 01:27:30 +0700

    change header

--- resources/views/normalisasi/index.blade.php
+++ resources/views/normalisasi/index.blade.php
@@ -5,39 +5,45 @@
 @endsection
 
 @section('content')
+    <!-- Content Header (Page header) -->
     <div class="content-header">
         <div class="container-fluid">
             <div class="row mb-2">
-
-                <div class="col-sm-6"></div>
-            </div>
-        </div>
+                <div class="col-sm-6">
+                    <h1 class="m-0">Perhitungan MOORA</h1>
+                </div><!-- /.col -->
+            </div><!-- /.row -->
+        </div><!-- /.container-fluid -->
     </div>
+    <!-- /.content-header -->
     <div class="content">
         <div class="container-fluid">
             <div class="row">
                 <div class="col-sm-12">
                     <div class="card">
+                        <div class="card-header">
+                            <h1 class="card-title">Normalisasi</h1>
+                        </div>
+                        <!-- /.card-header -->
                         <div class="card-body">
-                            <h2>Normalisasi</h2>
                             <table id="mytable" class="display nowrap table table-striped table-bordered">
                                 <thead>
-                                <tr>
-                                    <th>Alternatif</th>
-                                    @foreach ($kriteriabobot as $c)
-                                        <th>{{$c->nama}}</th>
-                                    @endforeach
-                                </tr>
-                                </thead>
-                                <tbody>
-                                @foreach ($alternatif as $a)
                                     <tr>
-                                        <td>{{$a->nama}}</td>
-                                        @foreach ($normalizedScores[$a->id] as $normalizedScore)
-                                            <td>{{ $normalizedScore }}</td>
+                                        <th>Alternatif</th>
+                                        @foreach ($kriteriabobot as $c)
+                                            <th>{{ $c->nama }}</th>
                                         @endforeach
                                     </tr>
-                                @endforeach
+                                </thead>
+                                <tbody>
+                                    @foreach ($alternatif as $a)
+                                        <tr>
+                                            <td>{{ $a->nama }}</td>
+                                            @foreach ($normalizedScores[$a->id] as $normalizedScore)
+                                                <td>{{ $normalizedScore }}</td>
+                                            @endforeach
+                                        </tr>
+                                    @endforeach
                                 </tbody>
                             </table>
                         </div>
@@ -51,28 +57,31 @@
             <div class="row">
                 <div class="col-sm-12">
                     <div class="card">
+                        <div class="card-header">
+                            <h1 class="card-title">Normalisasi Terbobot</h1>
+                        </div>
+                        <!-- /.card-header -->
                         <div class="card-body">
-                            <h2>Normalisasi Terbobot</h2>
                             <table id="mytable" class="display nowrap table table-striped table-bordered">
                                 <thead>
-                                <tr>
-
-                                    <th>Alternatif</th>
-                                    @foreach ($kriteriabobot as $k)
-                                        <th>{{$k->nama}}</th>
-                                    @endforeach
-                                </tr>
-                                </thead>
-                                <tbody>
-                                @foreach ($alternatif as $al)
                                     <tr>
 
-                                        <td>{{$al->nama}}</td>
-                                        @foreach ($normTerbobotResults[$al->id] as $result)
-                                            <td>{{ $result }}</td>
+                                        <th>Alternatif</th>
+                                        @foreach ($kriteriabobot as $k)
+                                            <th>{{ $k->nama }}</th>
                                         @endforeach
                                     </tr>
-                                @endforeach
+                                </thead>
+                                <tbody>
+                                    @foreach ($alternatif as $al)
+                                        <tr>
+
+                                            <td>{{ $al->nama }}</td>
+                                            @foreach ($normTerbobotResults[$al->id] as $result)
+                                                <td>{{ $result }}</td>
+                                            @endforeach
+                                        </tr>
+                                    @endforeach
                                 </tbody>
                             </table>
                         </div>
@@ -86,35 +95,38 @@
             <div class="row">
                 <div class="col-sm-12">
                     <div class="card">
+                        <div class="card-header">
+                            <h1 class="card-title">Nilai Optimasi</h1>
+                        </div>
+                        <!-- /.card-header -->
                         <div class="card-body">
-                            <h2>Nilai Optimasi</h2>
                             <table id="mytable" class="display nowrap table table-striped table-bordered">
                                 <thead>
-                                <tr>
-                                    <th>Alternatif</th>
-                                    <th>MAX</th>
-                                    <th>MIN</th>
-                                    <th>Nilai Yi</th>
-                                </tr>
+                                    <tr>
+                                        <th>Alternatif</th>
+                                        <th>MAX</th>
+                                        <th>MIN</th>
+                                        <th>Nilai Yi</th>
+                                    </tr>
                                 </thead>
                                 <tbody>
-                                @foreach ($hitungNilaiAkhir['benefit'] as $alternatifId => $benefitValue)
-                                    <tr>
-                                        <td>{{ $alternatif->where('id', $alternatifId)->first->nama['nama'] }}</td>
-                                        <td>{{ $benefitValue }}</td>
-                                        <td>
-                                            @if (isset($hitungNilaiAkhir['cost'][$alternatifId]))
-                                                {{ $hitungNilaiAkhir['cost'][$alternatifId] }}
-                                            @endif
-                                        </td>
-                                        <td>
-                                            @if (isset($hitungYi[$alternatifId]['yiValue']))
-                                                {{ $hitungYi[$alternatifId]['yiValue'] }}
-                                            @endif
-                                        </td>
+                                    @foreach ($hitungNilaiAkhir['benefit'] as $alternatifId => $benefitValue)
+                                        <tr>
+                                            <td>{{ $alternatif->where('id', $alternatifId)->first->nama['nama'] }}</td>
+                                            <td>{{ $benefitValue }}</td>
+                                            <td>
+                                                @if (isset($hitungNilaiAkhir['cost'][$alternatifId]))
+                                                    {{ $hitungNilaiAkhir['cost'][$alternatifId] }}
+                                                @endif
+                                            </td>
+                                            <td>
+                                                @if (isset($hitungYi[$alternatifId]['yiValue']))
+                                                    {{ $hitungYi[$alternatifId]['yiValue'] }}
+                                                @endif
+                                            </td>
 
-                                    </tr>
-                                @endforeach
+                                        </tr>
+                                    @endforeach
                                 </tbody>
 
                             </table>
@@ -125,21 +137,20 @@
             </div>
         </div>
     </div>
-
 @endsection
 @section('script')
-<script>
-    $(function () {
-        $('[data-toggle="tooltip"]').tooltip()
-        $('#mytable').DataTable({
-            "paging": true,
-            "lengthChange": false,
-            "searching": false,
-            "ordering": true,
-            "info": true,
-            "autoWidth": false,
-            "responsive": true,
+    <script>
+        $(function() {
+            $('[data-toggle="tooltip"]').tooltip()
+            $('#mytable').DataTable({
+                "paging": true,
+                "lengthChange": false,
+                "searching": false,
+                "ordering": true,
+                "info": true,
+                "autoWidth": false,
+                "responsive": true,
+            });
         });
-    });
-</script>
+    </script>
 @endsection
